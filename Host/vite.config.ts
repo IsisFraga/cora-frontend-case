fc881333537c76d91cefe9412b5d855adef7a4e9
Isis Fraga <51866290+IsisFraga@users.noreply.github.com>
2024-10-17 14:26:10 -0300

    feat: folder structure adjustments

---
+++ Host/vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import federation from '@originjs/vite-plugin-federation';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: 'host-app',
+      remotes: {
+        todoApp: 'http://localhost:5001/assets/remoteEntry.js',
+        bankApp: 'http://localhost:5002/assets/remoteEntry.js',
+        loginApp: 'http://localhost:5003/assets/remoteEntry.js',
+      },
+      shared: ['react', 'react-dom', 'react-router-dom'],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
