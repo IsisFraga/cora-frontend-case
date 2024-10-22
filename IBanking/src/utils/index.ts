8ade0a1d917fa24a9c9eb569e2dc5b9abbd378cb
Isis Fraga <51866290+IsisFraga@users.noreply.github.com>
2024-10-22 08:04:20 -0300

    fix: calculateDayBalance calc

--- IBanking/src/utils/index.ts
+++ IBanking/src/utils/index.ts
@@ -2,7 +2,7 @@ import { Transaction } from "../types";
 
 export const calculateDayBalance = (transactions: Transaction[]): number => {
   return transactions.reduce(
-    (sum, t) => sum + (t.entry === "CREDIT" ? t.amount : -t.amount),
+    (sum, t) => sum + (t.entry === "CREDIT" || t.entry === "REFUND" ? t.amount : -t.amount),
     0
   );
 };
