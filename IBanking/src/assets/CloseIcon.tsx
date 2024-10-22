4dd25ae6337164d2238fceb1de2515ab73309113
Isis Fraga <51866290+IsisFraga@users.noreply.github.com>
2024-10-22 07:49:41 -0300

    feat: adding assets

---
+++ IBanking/src/assets/CloseIcon.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+interface CloseIconProps {
+  className?: string;
+}
+
+export const CloseIcon: React.FC<CloseIconProps> = ({ className }) => (
+  <svg 
+    width="12" 
+    height="12" 
+    viewBox="0 0 24 24" 
+    fill="none" 
+    xmlns="http://www.w3.org/2000/svg"
+    className={className}
+  >
+    <path 
+      d="M18 6L6 18" 
+      stroke="currentColor" 
+      strokeWidth="2" 
+      strokeLinecap="round" 
+      strokeLinejoin="round"
+    />
+    <path 
+      d="M6 6L18 18" 
+      stroke="currentColor" 
+      strokeWidth="2" 
+      strokeLinecap="round" 
+      strokeLinejoin="round"
+    />
+  </svg>
+);
